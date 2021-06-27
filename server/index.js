72fb76b1bbea688e22ea9aa7db580d6dc4eb2f69
tligodsp <43437080+tligodsp@users.noreply.github.com>
2021-06-27 23:59:34 +0700

    commit heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import upsertMany from '@meanie/mongoose-upsert-many';
 import cors from 'cors';
+import path from 'path';
 
 import postRoutes from './routes/posts.js';
 import sinhVienRoutes from './routes/sinhViens.js';
@@ -43,13 +44,21 @@ app.use('/users', userRoutes);
 app.use('/task-logs', taskLogRoutes);
 app.use('/reports', reportRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin123456@cluster0.cjuu6.mongodb.net/QLKL?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
+const CONNECTION_STRING = 'mongodb+srv://admin:admin123456@cluster0.cjuu6.mongodb.net/QLKL?retryWrites=true&w=majority';
 
 mongoose.plugin(upsertMany);
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB has been connected"))
   .catch((error) => console.log(error));
 
 mongoose.set('useFindAndModify', false);
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`);
