952c441dccd1a11aac3ae4ee1e72b25c44ec4fd1
tligodsp <43437080+tligodsp@users.noreply.github.com>
2021-06-28 01:07:35 +0700

    commit message

--- server/index.js
+++ server/index.js
@@ -61,9 +61,9 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING, { useNewUrlParser: true,
 
 mongoose.set('useFindAndModify', false);
 
-// Step 1:
+// Step 1
 app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
+// Step 2
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
